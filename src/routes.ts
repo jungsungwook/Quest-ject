c3baa17d42f0f1c7130d07c040a762f8cd5723e6
jungsungwook <jswcyber@naver.com>
2023-05-30 09:47:10 +0900

    delete: react

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,6 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import apiRouter from "./api/api.routes.ts"
-import pageRouter from "./pages/pages.routes.ts";
 const router = new Router();
-
-router.use("/v1", pageRouter.routes(), pageRouter.allowedMethods());
 
 router.use("/api", apiRouter.routes(), apiRouter.allowedMethods());
 

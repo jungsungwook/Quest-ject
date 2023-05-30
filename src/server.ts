cfe572fb8a00cb9f01536c6218b0d53048c16b49
jungsungwook <jswcyber@naver.com>
2023-05-30 10:29:27 +0900

    feat: swagger

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,34 @@ import "https://deno.land/x/dotenv/load.ts";
 import router from "./routes.ts";
 import { Connect } from "./utils/database.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import { swaggerDoc } from "https://deno.land/x/deno_swagger_doc/mod.ts";
 
 await Connect();
 const app = new Application();
+
+const swaggerDefinition = {
+    info: {
+        title: 'Quest-JECT API', // Title (required)
+        version: '1.0.0', // Version (required)
+    },
+    host: `localhost:8000`, // Host (optional)
+    basePath: '/', // Base path (optional)
+};
+
+const options = {
+    swaggerDefinition,
+    apis: ['./api/**/*.ts'],
+};
+
+const swagger = await swaggerDoc(options);
+
+app.use(async (ctx, next) => {
+    if (ctx.request.url.pathname === "/swagger.json") {
+        ctx.response.body = swagger;
+    } else {
+        await next();
+    }
+});
 
 app.use(async (ctx, next) => {
     const responseHeaders = ctx.response.headers;
